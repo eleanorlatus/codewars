702d098863ad2de39d0ae5ba36726353112862c3
eleanorlatus <90465357+eleanorlatus@users.noreply.github.com>
2022-07-19 17:12:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ function multiply(n1,n2){
 function otherAngle(a, b) {
   return 180-a-b
 }
+
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(n) {
+  return String(n).split("").reverse().map((num)=>{
+  return Number(num)
+})
+  }
