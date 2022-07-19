a746cfcf473a6c2cfbc3708f6d10e7d46c9f593e
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-07-19 17:33:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -24,3 +24,16 @@ function digitize(n) {
 function greet(name){
   return `Hello, ${name} how are you doing today?`
 }
+
+//You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+function getMiddle(s)
+{
+let strlength = s.length
+if(strlength%2!==0){
+  return s.charAt(strlength/2)
+}
+  else{
+    return s.charAt((strlength/2)-1) + s.charAt(strlength/2)
+  }
+}
