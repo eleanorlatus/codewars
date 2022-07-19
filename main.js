7caac2fe4920d4e4e36f62cbd8f39ea28491f2a1
eleanorlatus <90465357+eleanorlatus@users.noreply.github.com>
2022-07-19 17:07:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,10 @@
 function multiply(n1,n2){
   return n1*n2
 }
+
+//You are given two interior angles (in degrees) of a triangle.
+//Write a function to return the 3rd.
+
+function otherAngle(a, b) {
+  return 180-a-b
+}
