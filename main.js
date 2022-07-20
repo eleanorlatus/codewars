9b6843a2ed2cca6e2860e04f930819b6d0fe01e2
Eleanor Latus <eleanor@latus.co.uk>
2022-07-20 19:03:43 +0100

    new codewars

--- main.js
+++ main.js
@@ -38,3 +38,14 @@ if(strlength%2!==0){
     return s.charAt((strlength/2)-1) + s.charAt(strlength/2)
   }
 }
+
+//Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+function enough(cap, on, wait) {
+  if((on+wait)<=cap){
+    return 0
+  }
+  else{
+    return ((cap-wait-on)*-1)
+  }
+}
