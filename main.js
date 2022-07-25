051244a02154fb4865f3353ee20b9ed0cc0986bd
Eleanor Latus <eleanor@latus.co.uk>
2022-07-25 19:06:22 +0100

    reverse string

--- main.js
+++ main.js
@@ -153,3 +153,9 @@ function friend(friends){
   function rowSumOddNumbers(n) {
     return n * n *n
   }
+
+  //Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+  function reverseWords(str) {
+    return str.split("").reverse().join("").split(" ").reverse().join(" ")  
+   }
