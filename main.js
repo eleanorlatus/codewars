f8d8ced723498377f68709d51f4b84d997036663
Eleanor Latus <eleanor@latus.co.uk>
2022-07-25 19:51:18 +0100

    new

--- main.js
+++ main.js
@@ -180,3 +180,18 @@ function friend(friends){
     //OR
 
     const solution = nums => nums===null ? [] : nums.sort((a,b)=>a-b)
+
+    //Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+    
+    function arithmetic(a, b, operator){
+      if(operator==="add"){
+        return a+b
+      }else if(operator==="subtract"){
+        return a-b
+      }else if(operator==="multiply"){
+        return a*b
+      }
+      else{
+        return a/b
+      }
+    }
