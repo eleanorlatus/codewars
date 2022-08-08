42a2eee7235ce6dfb17caedecacdaaf6ecb15730
Eleanor Latus <eleanor@latus.co.uk>
2022-08-08 20:29:19 +0100

    new

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 
+//Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. 
+//Essentially, rearrange the digits to create the highest possible number.
 
-
-
-
+function descendingOrder(n){
+  let x = n.toString()
+  let y = x.split("").sort((a,b)=>b-a).join("")
+  return Number(y)
+}
 
 const flip=(d, a)=>{
   //TODO
