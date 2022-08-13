c24a00c8dcc72c4d553c83f74add395b50e04071
Eleanor Latus <eleanor@latus.co.uk>
2022-08-13 10:09:07 +0100

    new

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+//Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 
+function XO(str) {
+  let x = str.match(/x/gi);
+  let o = str.match(/o/gi);
+  return (x && x.length) === (o && o.length);
+}
 //Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. 
 //Essentially, rearrange the digits to create the highest possible number.
 
