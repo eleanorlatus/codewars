add989c921be16315dfaa86b96ed349ab8ef02e9
Eleanor Latus <eleanor@latus.co.uk>
2022-08-13 10:14:00 +0100

    new

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+//You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+function likes(names) {
+  if(names.length===0){
+    return "no one likes this"
+  }
+  else if(names.length===1){
+    return `${names[0]} likes this`
+  }
+  else if(names.length===2){
+    return `${names[0]} and ${names[1]} like this`
+  }
+  else if(names.length===3){
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  }
+  else{
+    return `${names[0]}, ${names[1]} and ${names.length-2} others like this`
+  }
+}
+
 //Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 
 function XO(str) {
