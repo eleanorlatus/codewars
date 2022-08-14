756aa5c37f7714e451d9d9b2cc406e5804def916
Eleanor Latus <eleanor@latus.co.uk>
2022-08-14 18:27:25 +0100

    filter

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(l) {
+  return l.filter(x => typeof x === 'number')
+ }
+
 //fizzbuzz
 
-// Return an array
 function fizzbuzz(n){
   let arr=[]
   for(let i=1; i<=n;i++){
