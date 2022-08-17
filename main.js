14b087a23e1bd906d022a3961bb2ad50aaa35007
Eleanor Latus <eleanor@latus.co.uk>
2022-08-17 19:44:31 +0100

    square every digit

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+
+//square every digit
+
+function squareDigits(num){
+  let arr = String(num).split("")
+  let newArr= []
+  for(let i=0;i<arr.length;i++){
+    let num = Number(arr[i])
+    newArr.push(num*num)
+}
+let result = newArr.join("")
+return Number(result)
+  }
+  
+  //Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
 function countSheeps(arrayOfSheep) {
   let sheep = 0
