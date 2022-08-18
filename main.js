bebe5fead3ab7549bc759f8b08301547318a8ff1
Eleanor Latus <eleanor@latus.co.uk>
2022-08-18 18:55:55 +0100

    accum

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+function accum(s) {
+  return s.toLowerCase().split("").map((x,i) => x[0].toUpperCase() + x.repeat(i)).join("-")
+ }
 
 //square every digit
 
