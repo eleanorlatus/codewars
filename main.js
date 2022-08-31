c35d919d4e5e9d49de57805689c5ef54cd8b89da
Eleanor Latus <eleanor@latus.co.uk>
2022-08-31 19:58:45 +0100

    array problems

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+function calcuate(arr){
+  return arr.filter((x,i)=>x%i==0)
+  }
+  
+  console.log(calcuate([22, -6, 32, 82, 9, 25]))
+  
+  //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+  
+  function sumArray(arr){
+  return arr.reduce((a,b)=>Number(a)+Number(b),0)
+  }
+  
+  console.log(sumArray(["1",4,3,"2"]))
+
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
 
