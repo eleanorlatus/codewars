cd1e3d92291611241628da46b1a50fa4c66925b6
Eleanor Latus <eleanor@latus.co.uk>
2022-08-31 19:57:49 +0100

    array problems

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+//Arrays
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+
+function reversePokemonList(list){
+  console.log(list.reverse())
+  }
+  
+  reversePokemonList(["pikachu","bulbasaur","snorlax"])
+
+  //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+
+function evaluate(a,b){
+  return a.reduce((a,b)=>a+b**2,0) > b.reduce((a,b)=>a+b**3,0)
+  }
+  
+  console.log(evaluate([1,2,20],[3,4,5]))
+
 // Variables
 //You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
 
