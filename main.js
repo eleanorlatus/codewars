8f8f0cfe7d4dd184dce76562a70209400e261745
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-09-04 16:55:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//convert km/h to cm/s
+
+function cockroachSpeed(s) {
+  return Math.floor((s*100000)/3600)
+}
+
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
