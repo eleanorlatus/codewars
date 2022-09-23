e0fd4a40c7d0d3745a8c7fd2702591c3b6225224
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-09-23 18:00:00 +0100

    Create name_shuffler.js

---
+++ 8KYU/name_shuffler.js
@@ -0,0 +1,6 @@
+function nameShuffler(str) {
+let convertoToArray = str.split(" ");
+let reverseArray = convertoToArray.reverse(" ");
+let convertToString = reverseArray.join(" ");
+  return convertToString
+}
