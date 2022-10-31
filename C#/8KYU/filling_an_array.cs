f696b91f7b075405e051bb7838c24df4fa83af9f
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-10-31 15:43:31 +0000

    Create filling_an_array.cs

---
+++ C#/8KYU/filling_an_array.cs
@@ -0,0 +1,12 @@
+public class Kata
+{
+  public static int[] Arr(int N)
+  {
+      int[] result = new int[N];
+      for(int i =0; i < N; i++)
+        {
+        result[i] = i;
+      }
+      return result;
+  }
+}
