c3b00cef37030f3b1da03a0c7e9e75a2a9bcc071
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-10-31 15:05:40 +0000

    Create grasshopper_array.cs

---
+++ C#/8KYU/grasshopper_array.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class GrassHopper 
+    {
+        public static int FindAverage(int[] nums)
+        {
+          int sum = 0;
+             for(int i = 0; i < nums.Length; i++)
+               {
+               sum+=nums[i];
+             }
+          return sum / nums.Length;
+        }
+    }
+    
+    
+    // OR
+    
+    using System;
+
+public class GrassHopper 
+    {
+        public static int FindAverage(int[] nums)
+        {
+          int sum = 0;          
+          foreach (int num in nums)
+            {
+            sum+=num;
+          }
+          return sum / nums.Length;
+        }
+    }
