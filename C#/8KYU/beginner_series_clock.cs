d77387c73455d03fda76f45b9daa269ea29d7dbc
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-11-04 11:49:29 +0000

    Create beginner_series_clock.cs

---
+++ C#/8KYU/beginner_series_clock.cs
@@ -0,0 +1,11 @@
+  using System;
+  public static class Clock
+  {
+    public static int Past(int h, int m, int s)
+    {
+      int hour = h * 3600000;
+      int min = m * 60000;
+      int sec = s*1000;
+      return hour + min + sec;
+    }
+  }
