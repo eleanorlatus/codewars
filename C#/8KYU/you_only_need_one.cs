8e495915d8069c5e7abe741f5327794a3cb824f7
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-11-08 15:07:07 +0000

    Create you_only_need_one.cs

---
+++ C#/8KYU/you_only_need_one.cs
@@ -0,0 +1,15 @@
+using System.Linq;
+
+public class Kata
+{
+  public static bool Check(object[] a, object x)
+  {
+  if(a.Contains(x))
+    {
+    return true;
+  }else
+    {
+    return false;
+  }
+  }
+}
