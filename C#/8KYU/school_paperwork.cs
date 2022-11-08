421e373805fc4a6aaeba76cf9ca9c5bfc2d10089
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-11-08 15:15:03 +0000

    Create school_paperwork.cs

---
+++ C#/8KYU/school_paperwork.cs
@@ -0,0 +1,23 @@
+  using System;
+  public static class Paper
+  {
+    public static int Paperwork(int n, int m) => n < 0 || m < 0 ? 0 : n*m;
+      
+}
+      
+// OR
+
+    using System;
+  public static class Paper
+  {
+    public static int Paperwork(int n, int m)
+   {
+      if(n < 0 || m < 0)
+      {
+        return 0;
+      }else
+        {
+        return m*n;
+      }
+    }
+  }
