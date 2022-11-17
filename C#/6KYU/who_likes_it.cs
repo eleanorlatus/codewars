0111e751926c9bfe4836462f68244efe168a54c7
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-11-17 15:56:10 +0000

    Update who_likes_it.cs

--- C#/6KYU/who_likes_it.cs
+++ C#/6KYU/who_likes_it.cs
@@ -23,3 +23,20 @@ public static class Kata
     }
   }
 }
+
+// OR
+
+public static class Kata
+{
+  public static string Likes(string[] names)
+  {
+    switch (names.Length)
+    {
+      case 0: return "no one likes this"; // :(
+      case 1: return $"{names[0]} likes this";
+      case 2: return $"{names[0]} and {names[1]} like this";
+      case 3: return $"{names[0]}, {names[1]} and {names[2]} like this";
+      default: return $"{names[0]}, {names[1]} and {names.Length - 2} others like this";
+    }
+  }
+}
