de12873879fbbddf5fbd34eeac1eb9ce2a0f73ae
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2022-11-21 11:46:56 +0000

    Create return_month_name.cs

---
+++ C#/8KYU/return_month_name.cs
@@ -0,0 +1,8 @@
+public class Program 
+{
+    public static string MonthName(int num) 
+    {
+			string[] months = {"January", "February","March","April","May","June","July","August","September","October","November","December"};
+			return months[num-1];
+    }
+}
