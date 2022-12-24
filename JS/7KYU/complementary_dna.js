5963046070a127c801bd52f1def42a2db4469d9d
Eleanor Latus <eleanor@latus.co.uk>
2022-12-24 13:07:55 +0000

    added coding challenge

---
+++ JS/7KYU/complementary_dna.js
@@ -0,0 +1,15 @@
+function DNAStrand(dna){
+    let result = []
+    for(let i = 0 ; i < dna.length; i++){
+      if(dna[i]=="A"){
+        result.push("T")
+      }else if(dna[i]=="T"){
+       result.push("A")
+      }else if(dna[i]=="G"){
+        result.push("C")
+      }else{
+       result.push("G")
+      }
+    }
+    return result.join("")
+  }
