99f97938357ac29eea71ef6a542e648ef4470f83
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2023-01-08 11:20:52 +0000

    Update sum_of_a_sequence.js

--- JS/7KYU/sum_of_a_sequence.js
+++ JS/7KYU/sum_of_a_sequence.js
@@ -1,37 +1,3 @@
-const sequenceSum = (b, e, s) => {
-let sum = [b]
-if(b>e){
-    return 0
-}else{
-  while(b <= e){
-  b+=s
-  if(b<=e){
-   sum.push(b)
-  }
-}
-  return sum.reduce((a,b)=> a+b,0)
-}
-}
-
-//OR
-
-const sequenceSum = (b, e, s) => {
-let sum = b
-if(b>e){
-    return 0
-}else{
-  while(b <= e){
-  b+=s
-  if(b<=e){
-   sum+=b
-  }
-}
-  return sum
-}
-}
-
-// OR
-
 const sequenceSum = (begin, end, step) => {
   var sum = 0;
   for(var i=begin;i<=end;i+=step)
