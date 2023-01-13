dccb0d6f648b54f6bf1fa2670cebd1e0f75a8ec6
Eleanor Latus <eleanor@latus.co.uk>
2023-01-13 10:10:53 +0000

    refined solution

--- JS/6KYU/find_missing_letter.js
+++ JS/6KYU/find_missing_letter.js
@@ -4,3 +4,11 @@ function findMissingLetter(array)
    array.forEach( x =>  x.charCodeAt() == i ? i++ : i );
    return String.fromCharCode(i);
 }
+
+// OR
+
+const findMissingLetter = (array) => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const start = alphabet.indexOf(array[0]);
+    return alphabet.slice(start, start + array.length).find(el => !array.includes(el));
+  };
