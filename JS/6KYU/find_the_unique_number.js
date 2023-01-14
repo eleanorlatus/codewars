549226fe8472a65b01aa334c58d1151557f41d5b
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2023-01-14 16:32:23 +0000

    Update find_the_unique_number.js

--- JS/6KYU/find_the_unique_number.js
+++ JS/6KYU/find_the_unique_number.js
@@ -1,3 +1,11 @@
 function findUniq(arr) {
 return Number(arr.filter((x) => arr.indexOf(x)==arr.lastIndexOf(x)))
 }
+
+
+// OR
+
+function findUniq(arr) {
+  arr.sort((a,b)=>a-b);
+  return arr[0]==arr[1]?arr.pop():arr[0]
+}
