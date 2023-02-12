f55370e0765101f425d7f4de244cc9de8b9e31bb
Eleanor Latus <eleanor@latus.co.uk>
2023-02-12 11:44:22 +0000

    changes made

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function cockroachSpeed(s) {
 }
 
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
-// Some cases:
+// Some test cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 
