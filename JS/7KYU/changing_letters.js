12036c3624bcd31d02cb3eddd71ab352ebd98dc9
Eleanor Latus <eleanor@latus.co.uk>
2023-03-03 10:54:26 +0000

    new codewars

--- JS/7KYU/changing_letters.js
+++ JS/7KYU/changing_letters.js
@@ -2,3 +2,9 @@ function swap (string) {
     const vowels = ['a','e','i','o','u']
     return string.split("").map((x) => vowels.includes(x) ? x.toUpperCase() : x).join("")
     }
+
+    //using regex
+
+    function swap(st){
+        return st.replace(/[aeiou]/g, v => v.toUpperCase() )
+      }
