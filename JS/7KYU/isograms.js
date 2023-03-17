41a37f8ee19a6331a17f1fa6b3ecb9406a3eea80
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2023-03-17 10:09:04 +0000

    Update isograms.js

--- JS/7KYU/isograms.js
+++ JS/7KYU/isograms.js
@@ -1,3 +1,10 @@
 function isIsogram(str){
   return !/(.).*\1/.test(str.toLowerCase());
 }
+
+// OR
+
+function isIsogram(str){
+const arr = str.toLowerCase().split("")
+return arr.filter((x,i) => arr.indexOf(x) != arr.lastIndexOf(x)).length > 0 ? false : true
+}
