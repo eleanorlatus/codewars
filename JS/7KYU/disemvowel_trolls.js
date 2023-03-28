0ff9dacde7941df76fa4867aca994eac97cf7db1
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2023-03-28 15:15:06 +0100

    Update disemvowel_trolls.js

--- JS/7KYU/disemvowel_trolls.js
+++ JS/7KYU/disemvowel_trolls.js
@@ -1,3 +1,10 @@
 function disemvowel(str) {
+  const vowels = "aeiou"
+  return str.split("").filter((x) => !vowels.includes(x.toLowerCase())).join("")
+}
+
+// OR
+
+function disemvowel(str) {
   return str.replace(/[aeiou]/gi, '');
 }
