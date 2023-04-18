20a224b6dc32e3379a505d47872bb020a692d3f7
Eleanor Latus <eleanor@latus.co.uk>
2023-04-18 14:07:53 +0100

    updated readme

--- JS/8KYU/remove_first_last_character_pt_2.js
+++ JS/8KYU/remove_first_last_character_pt_2.js
@@ -3,3 +3,9 @@ function array(string) {
     const result = arr.slice(1, arr.length-1).join(" ")
     return result.length > 0 ? result : null
 }
+
+// OR
+
+function array(string) {
+    return string.split(",").slice(1,-1).join(" ") || null
+    }
