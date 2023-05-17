1bea6fb9e6727f681fe2d119d89d1f829e47861c
Eleanor Latus <90465357+eleanorlatus@users.noreply.github.com>
2023-05-17 14:45:06 +0100

    Refactor exes_and_ohs.js

--- JS/7KYU/exes_and_ohs.js
+++ JS/7KYU/exes_and_ohs.js
@@ -17,3 +17,27 @@ function XO(str) {
     return false
   }
 }
+
+// OR
+
+function XO(str) {
+   const x = str.split("").filter((x) => x.toLowerCase() == 'x')
+   const o = str.split("").filter((x) => x.toLowerCase() == 'o')
+   
+   return x.length == o.length
+}
+
+// OR
+
+const XO = str => {
+  str = str.toLowerCase().split('');
+  return str.filter(x => x === 'x').length === str.filter(x => x === 'o').length;
+}
+
+// OR
+
+function XO(str) {
+  let x = str.match(/x/gi);
+  let o = str.match(/o/gi);
+  return (x && x.length) === (o && o.length);
+}
